1d2559e954ad0333c57e5b5cc9376baa4b1cd7f7
Haruyoshi Gonsho <robi10@outlook.jp>
2025-11-23 19:02:56 +0900

    管理者ログインエラー処理

--- src/main/java/com/todo/app/controller/admin/AdminController.java
+++ src/main/java/com/todo/app/controller/admin/AdminController.java
@@ -1,5 +1,7 @@
 package com.todo.app.controller.admin;
 
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -7,6 +9,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.todo.app.entity.User;
@@ -31,8 +34,14 @@ public class AdminController {
 	@GetMapping({ "/", "/login", "/logout" })
 	public String showAdminPage(
 			LoginForm loginForm,
+			@RequestParam(name = "error", defaultValue = "") String error,
+			HttpSession session,
 			Model model) {
-		// 管理者画面の表示ロジックをここに実装
+			// 管理者画面の表示ロジックをここに実装
+			session.invalidate();
+			if (error.equals("notLoggedIn")) {
+				model.addAttribute("message", "ログインしてください");
+			}
 		return "/admin/login";
 	}
 
